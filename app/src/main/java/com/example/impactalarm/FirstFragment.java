525bf1e708e69ac197cf2fa2e278633c0df9e81f
Keifer-Schmueser <52268060+Keifer-Schmueser@users.noreply.github.com>
2021-04-17 01:39:41 -0500

    Sample request and new button

--- app/src/main/java/com/example/impactalarm/FirstFragment.java
+++ app/src/main/java/com/example/impactalarm/FirstFragment.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class FirstFragment extends Fragment {
 
@@ -28,6 +35,40 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                URL url = null;
+                try {
+                    url = new URL("https://reqres.in/api/users?page=2");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                HttpURLConnection connection = null;
+                try {
+                    connection = (HttpURLConnection) url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    connection.setRequestMethod("GET");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                }
+
+                InputStream responseStream = null;
+                try {
+                    responseStream = connection.getInputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(responseStream);
             }
         });
     }
